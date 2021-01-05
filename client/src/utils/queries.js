ce9b5a869bd1761454c191ac4cbb859e2d1026f3
William Santee <william.santee@gmail.com>
2021-01-05 16:58:55 -0700

    implemented GraphQL queries and navigation for other pages.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
@@ -13,6 +13,45 @@ export const QUERY_THOUGHTS = gql`
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
